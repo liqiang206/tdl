4fb6c7a6864e790f551fa357fd2856b654e45702
liqiang206 <jianganzzx@outlook.com>
2026-03-08 08:16:53 +0800

    Update options.go

--- app/dl/options.go
+++ app/dl/options.go
@@ -1,1 +1,25 @@
+package dl
 
+type Options struct {
+    Dir        string
+    RewriteExt bool
+    SkipSame   bool
+    Template   string
+    URLs       []string
+    Files      []string
+    Include    []string
+    Exclude    []string
+    Desc       bool
+    Takeout    bool
+    Group      bool
+
+    // resume opts
+    Continue, Restart bool
+
+    // serve
+    Serve bool
+    Port  int
+
+    // ===== 新增字段 =====
+    Stdout bool // 是否输出到标准输出
+}
